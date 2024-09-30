e907f52f8bb0249abc4910d71faeeb6800723012
ymushet <egor.mushet@gmail.com>
2024-09-30 21:38:52 +0300

    hotfix: remove var from backend s3

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   backend "s3" {
     bucket         = "rsschool-devops-tfstate"
     key            = "state/terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "rsschool-devops-terraform-lock-table"
   }
