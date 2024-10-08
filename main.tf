c79c8e435282b0ad191cc9dc95d885aaead25b42
Yehor Mushet <egor.mushet@gmail.com>
2024-10-08 23:53:55 +0300

    feat: locked providers and tf versions

--- main.tf
+++ main.tf
@@ -3,9 +3,11 @@ provider "aws" {
 }
 
 terraform {
+  required_version = "1.9.6"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "5.68.0"
     }
   }
 
